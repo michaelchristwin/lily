ae5b5dc4c7e9eeb5ceddde893293563b145e0572
michaelchristwin <kelechukwuchristwin@gmail.com>
2026-07-13 12:04:27 +0100

    Added users service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+
+import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { CurrentUser } from '../auth/current-user.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+@UseGuards(JwtAuthGuard)
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('me')
+  me(@CurrentUser() user: { id: string }) {
+    return this.usersService.findById(user.id);
+  }
+
+  @Patch('me')
+  update(@CurrentUser() user: { id: string }, @Body() dto: UpdateUserDto) {
+    return this.usersService.update(user.id, dto);
+  }
+
+  @Delete('me')
+  remove(@CurrentUser() user: { id: string }) {
+    return this.usersService.remove(user.id);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,58 @@
-import { Injectable, Inject } from '@nestjs/common';
-//import { eq } from 'drizzle-orm';
+import { Injectable, Inject, NotFoundException } from '@nestjs/common';
+import { eq } from 'drizzle-orm';
 import { DRIZZLE, type DrizzleDB } from '../database/database.provider';
+import { usersTable } from '../database/schema';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(@Inject(DRIZZLE) private readonly db: DrizzleDB) {}
+  async findById(id: string) {
+    const user = await this.db.query.usersTable.findFirst({
+      where: { id: id },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return this.toUserResponse(user);
+  }
+
+  async update(id: string, dto: UpdateUserDto) {
+    const [user] = await this.db
+      .update(usersTable)
+      .set({
+        ...dto,
+        updatedAt: new Date(),
+      })
+      .where(eq(usersTable.id, id))
+      .returning();
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return this.toUserResponse(user);
+  }
+
+  async remove(id: string) {
+    const [user] = await this.db
+      .delete(usersTable)
+      .where(eq(usersTable.id, id))
+      .returning();
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return;
+  }
+
+  private toUserResponse(user: typeof usersTable.$inferSelect) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { passwordHash, ...safeUser } = user;
+
+    return safeUser;
+  }
 }
